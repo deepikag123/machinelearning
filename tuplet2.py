737f28e0428a285057751f695878db3d698a2726
Deepika G <143984683+deepikag123@users.noreply.github.com>
2024-07-08 22:40:03 +0530

    Add files via upload

---
+++ tuplet2.py
@@ -0,0 +1,17 @@
+stu=("deep","female",1,2,3,4)
+print(stu)
+n=stu.count(stu)
+print("count:",n)
+print("lenght:",len(stu))
+i=stu.index("female")
+print("index:",i)
+a=list(stu)
+print("list:",a)
+
+
+
+
+
+
+
+
